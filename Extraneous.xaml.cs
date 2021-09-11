e10fa888eb61841e4760cad70fe9355bacf5fb1f
mohammad reza jafari <mohammad.mrjr@gmail.com>
2021-09-11 19:17:24 +0430

    Adding Sources

---
+++ Extraneous.xaml.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using sqlite_utility;
+namespace import_export
+{
+    public partial class TemplateWin : Window
+    {
+        PersianCalendar cal = new PersianCalendar();
+        protected Sqlite_Utility sqlite = new Sqlite_Utility("datas.db");
+        protected string Table_key { get; set; }
+        protected int[] date_formatted;
+        public TemplateWin(string key)
+        {
+            InitializeComponent();
+            Table_key = key;
+        }
+        public virtual void Textbox_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender.Equals(date))
+            {
+                date.Foreground = Brushes.Black;
+                date.Text = $"{cal.GetYear(DateTime.Now)}/{cal.GetMonth(DateTime.Now)}/{cal.GetDayOfMonth(DateTime.Now)}" +
+                    $" - {cal.GetHour(DateTime.Now)}:{cal.GetMinute(DateTime.Now)}";
+            }
+            else if (sender.Equals(name))
+            {
+                name.Text = "";
+                name.Foreground = Brushes.Black;
+            }
+            else if (sender.Equals(quantity))
+            {
+                quantity.Text = "";
+                quantity.Foreground = Brushes.Black;
+            }
+            else if (sender.Equals(cost))
+            {
+                cost.Text = "";
+                cost.Foreground = Brushes.Black;
+            }
+            else if (sender.Equals(details))
+            {
+                details.Text = "";
+                details.Foreground = Brushes.Black;
+            }
+        }
+        public  virtual void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public bool Regex_Check(string pattern, string input)
+        {
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(input);
+        }
+        public bool Date_Check()
+        {
+            char[] split = new char[] { '/', '-', ':' };
+            string[] date_splited;
+            date_splited = date.Text.Split(split);
+            date_formatted = Array.ConvertAll(date_splited, int.Parse);
+            if (date_formatted[1] < 13 && date_formatted[1] > 0)
+            {
+                if (date_formatted[2] > 0 && date_formatted[2] < 32)
+                {
+                    if (date_formatted[3] < 24 && date_formatted[3] >= 0)
+                    {
+                        if (date_formatted[4] < 60 && date_formatted[4] >= 0)
+                        {
+                            return true;
+                        }
+                        else
+                            return false;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+    public partial class Add : TemplateWin
+    {
+        public Add(string key) : base(key)
+        {                       
+            Title = "Add";
+            confirm.Content = FindResource("confirm_pic");            
+        }        
+        public override void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            if (name.Text != "" && name.Text != "Item Name")
+            {
+                if (Regex_Check(@"^\d{4}\s{0,1}\/\s{0,1}\d{1,2}\s{0,1}\/\s{0,1}\d{1,2}\s{0,1}-\s{0,1}\d{1,2}\s{0,1}\:\s{0,1}\d{1,2}$", date.Text) && Date_Check())
+                {
+                    if (quantity.Text != "0" && Regex_Check(@"^\d{1,}", quantity.Text))
+                    {
+                        if (Regex_Check(@"^\d{1,}", cost.Text))
+                        {
+                            sqlite.sql_command($"INSERT INTO {Table_key} ( item , quantity , year , month , day ,hour , minute  , cost , notes ) VALUES (\"{name.Text}\" ," +
+                                $" {int.Parse(quantity.Text)} , {date_formatted[0]} , {date_formatted[1]} , {date_formatted[2]} , {date_formatted[3]} , {date_formatted[4]} ," +
+                                $" {int.Parse(cost.Text)} , \"{(details.Text != "Details (Max 300 characters)" ? details.Text : "")}\" )");
+                            this.Close();
+                        }
+                        else
+                            MessageBox.Show("Invalid Cost .");
+                    }
+                    else
+                        MessageBox.Show("Invalid Quantity .");
+                }
+                else
+                    MessageBox.Show("Invalid Date .");
+            }
+            else
+                MessageBox.Show("Invalid Name .");
+        }       
+    }
+    public partial class Item_description : TemplateWin
+    {
+        private Int32 ID;
+        public Item_description(string key , Data_Binding data) : base(key)
+        {
+            Title = "Description";
+            confirm.Content = FindResource("update_pic");
+            ID = data.ID;
+            name.Text = data.Item;
+            date.Text = data.Date;
+            quantity.Text = data.Quantity.ToString();
+            cost.Text = data.Cost.ToString();
+            sqlite.sql_command_read($"SELECT notes FROM {Table_key} WHERE ID = {data.ID}");
+            while(sqlite.check_reader_and_close())
+            {               
+                if (( details.Text = sqlite.reader.GetString(0) ) != "")                
+                    details.Foreground = Brushes.Gray;
+                else
+                    details.Foreground = Brushes.Black;
+            }            
+        }
+        public override void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            if (name.Text != "" && name.Text != "Item Name")
+            {
+                if (Regex_Check(@"^\d{4}\s{0,1}\/\s{0,1}\d{1,2}\s{0,1}\/\s{0,1}\d{1,2}\s{0,1}-\s{0,1}\d{1,2}\s{0,1}\:\s{0,1}\d{1,2}$", date.Text) && Date_Check())
+                {
+                    if (quantity.Text != "0" && Regex_Check(@"^\d{1,}", quantity.Text))
+                    {
+                        if (Regex_Check(@"^\d{1,}", cost.Text))
+                        {
+                            sqlite.sql_command($"UPDATE {Table_key} set item = \"{name.Text}\" , quantity = {int.Parse(quantity.Text)} , year = {date_formatted[0]} ," +
+                                $" month = {date_formatted[1]} , day = {date_formatted[2]} ,hour = {date_formatted[3]} , minute = {date_formatted[4]} ," +
+                                $" cost = {int.Parse(cost.Text)}  , notes = \"{(details.Text != "Details (Max 300 characters)" ? details.Text : "")}\" WHERE ID = {ID}"); 
+                            this.Close();
+                        }
+                        else
+                            MessageBox.Show("Invalid Cost .");
+                    }
+                    else
+                        MessageBox.Show("Invalid Quantity .");
+                }
+                else
+                    MessageBox.Show("Invalid Date .");
+            }
+            else
+                MessageBox.Show("Invalid Name .");
+        }
+        public override void Textbox_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender.Equals(date))
+            {
+                date.Foreground = Brushes.Black;
+            }
+            else if (sender.Equals(name))
+            {               
+                name.Foreground = Brushes.Black;
+            }
+            else if (sender.Equals(quantity))
+            {
+                quantity.Foreground = Brushes.Black;
+            }
+            else if (sender.Equals(cost))
+            {
+                cost.Foreground = Brushes.Black;
+            }
+            else if (sender.Equals(details))
+            {
+                details.Foreground = Brushes.Black;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using sqlite_utility;
+namespace import_export
+{
+    public partial class MainWindow : Window
+    {
+        PersianCalendar cal = new PersianCalendar();
+        Sqlite_Utility sqlite = new Sqlite_Utility("datas.db");
+        string Table_key { get; set; } = "import";
+        Int32 total ;
+        public MainWindow()
+        {
+            InitializeComponent();
+            add.Content = FindResource("add_pic");
+            change.Content = FindResource("change_pic");
+            search.Content = FindResource("search_pic");
+            print_date.Content = $"{cal.GetYear(DateTime.Now)} / {cal.GetMonth(DateTime.Now)} / {cal.GetDayOfMonth(DateTime.Now)} - {cal.GetDayOfWeek(DateTime.Now)}";
+            ListView_rearrange($"SELECT * FROM {Table_key} ORDER BY ID DESC");
+        }
+        private void ListView_rearrange(string command)
+        {
+            total = 0;
+            list.Items.Clear();
+            sqlite.sql_command_read(command);
+            while (sqlite.check_reader_and_close())
+            {
+                list.Items.Add(new Data_Binding
+                {
+                    ID = sqlite.reader.GetInt32(0),
+                    Item = sqlite.reader.GetString(1),
+                    Quantity = sqlite.reader.GetInt16(2),
+                    Date = $"{sqlite.reader.GetInt16(3)}" +
+                    $"/{sqlite.reader.GetInt16(4)}/{sqlite.reader.GetInt16(5)} - {sqlite.reader.GetInt16(6)}:{sqlite.reader.GetInt16(7)}",
+                    Cost = sqlite.reader.GetInt32(8),
+                    Details = (sqlite.reader.GetString(9) != "") ? "notes" : ""
+                });
+                total += sqlite.reader.GetInt32(8);
+            }
+            total_cost.Content = $"Total : {total.ToString()}";
+        }
+        private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            if(Table_key == "import")
+            {
+                Table_key = "export";
+                Table_name.Content = "EXPORT";
+            }
+            else
+            {
+                Table_key = "import";
+                Table_name.Content = "IMPORT";
+            }
+            ListView_rearrange($"SELECT * FROM {Table_key} ORDER BY ID DESC");
+        }       
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            TemplateWin form = new Add(Table_key);
+            form.ShowDialog();
+            ListView_rearrange($"SELECT * FROM {Table_key} ORDER BY ID DESC");
+        }
+        private void Search_Click(object sender , RoutedEventArgs e)
+        {
+            switch(Search_Parameters_Check())
+            {
+                case 0:
+                    MessageBox.Show("Invalid Search Parameters .");
+                    break;
+                case 1:
+                    ListView_rearrange($"SELECT * FROM {Table_key} ORDER BY ID DESC");
+                    break;
+                case 2:
+                    string searchQuery = $"SELECT * FROM {Table_key} WHERE ";
+                    if (c_name.IsChecked == true)
+                    {
+                        searchQuery += $"item LIKE \'%{t_name.Text}%\' ";
+                        if (c_year.IsChecked == true || c_month.IsChecked == true || c_day.IsChecked == true || c_quantity.IsChecked == true)
+                            searchQuery += $"AND ";
+                    }
+                    if (c_year.IsChecked == true)
+                    {
+                        searchQuery += $"year = {int.Parse(t_year.Text)} ";
+                        if (c_month.IsChecked == true || c_day.IsChecked == true || c_quantity.IsChecked == true)
+                            searchQuery += $"AND ";
+                    }
+                    if (c_month.IsChecked == true)
+                    {
+                        searchQuery += $"month = {int.Parse(t_month.Text)} ";
+                        if (c_day.IsChecked == true || c_quantity.IsChecked == true)
+                            searchQuery += $"AND ";
+                    }
+                    if (c_day.IsChecked == true)
+                    {
+                        searchQuery += $"day = {int.Parse(t_day.Text)} ";
+                        if (c_quantity.IsChecked == true)
+                            searchQuery += $"AND ";
+                    }
+                    if (c_quantity.IsChecked == true)
+                    {
+                        searchQuery += $"quantity = {int.Parse(t_quantity.Text)} ";
+                    }
+                    searchQuery += "ORDER BY ID DESC";
+                    ListView_rearrange(searchQuery);
+                    break;
+            }                  
+        }
+        private void CheckBox_Check_changed(object sender, RoutedEventArgs e)
+        {
+            if (sender.Equals(c_name))
+            {
+                if (c_name.IsChecked == true)                   
+                    t_name.IsEnabled = true;
+                else
+                {
+                    t_name.Text = "";
+                    t_name.IsEnabled = false;
+                }
+            }                
+            else if (sender.Equals(c_year))
+            {
+                if (c_year.IsChecked == true)
+                    t_year.IsEnabled = true;
+                else
+                {
+                    t_year.Text = "";
+                    t_year.IsEnabled = false;
+                }
+            }            
+            else if (sender.Equals(c_month))
+            {
+                if (c_month.IsChecked == true)
+                    t_month.IsEnabled = true;
+                else
+                {
+                    t_month.Text = "";
+                    t_month.IsEnabled = false;
+                }
+            }
+            else if (sender.Equals(c_day))
+            {
+                if (c_day.IsChecked == true)
+                    t_day.IsEnabled = true;
+                else
+                {
+                    t_day.Text = "";
+                    t_day.IsEnabled = false;
+                }
+            }
+            else if (sender.Equals(c_quantity))
+            {
+                if (c_quantity.IsChecked == true)
+                    t_quantity.IsEnabled = true;
+                else
+                {
+                    t_quantity.Text = "";
+                    t_quantity.IsEnabled = false;
+                }
+            }
+        }
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (list.SelectedItems.Count > 0)
+            {
+                TemplateWin form = new Item_description(Table_key, (Data_Binding)list.SelectedItem);
+                form.ShowDialog();
+                ListView_rearrange($"SELECT * FROM {Table_key} ORDER BY ID DESC");
+            }           
+        }
+        private int Search_Parameters_Check()
+        {
+            int status = 1;
+            if (c_name.IsChecked == true)
+            {
+                if (t_name.Text == "")
+                {
+                    return 0;
+                }
+                status = 2;
+            }
+            if (c_year.IsChecked == true)
+            {                
+                if (!Regex_Check(@"^\d{1,}$" , t_year.Text))
+                {
+                    return 0;
+                }
+                status = 2;
+            }
+            if (c_month.IsChecked == true)
+            {
+                if (!Regex_Check(@"^\d{1,}$", t_month.Text))
+                {
+                    return 0;
+                }
+                status = 2;
+            }
+            if (c_day.IsChecked == true)
+            {
+                if (!Regex_Check(@"^\d{1,}$", t_day.Text))
+                {
+                    return 0;
+                }
+                status = 2;
+            }
+            if (c_quantity.IsChecked == true)
+            {
+                if (!Regex_Check(@"^\d{1,}$", t_quantity.Text))
+                {
+                    return 0;
+                }
+                status = 2;
+            }
+            return status;
+        }
+        private bool Regex_Check(string pattern, string input)
+        {
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(input);
+        }
+    }
+    public class Data_Binding
+    {
+        public Int32 ID { get; set; }
+        public string Item { get; set; }
+        public int Quantity { get; set; }
+        public string Date { get; set; }
+        public Int32 Cost { get; set; }
+        public string Details { get; set; }
+    }
+}
